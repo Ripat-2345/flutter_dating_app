f35dd3f6bfb5fa82e4f1d52f5f1939c2993c6313
Ripat-2345 <ripatalsafar@gmail.com>
2023-02-13 14:37:20 +0700

    create sign in page

--- lib/src/features/authentication/presentation/sign_up_screen.dart
+++ lib/src/features/authentication/presentation/sign_up_screen.dart
@@ -2,10 +2,30 @@ import 'package:dating_app/src/theme_manager/values_manager.dart';
 import 'package:flutter/material.dart';
 
 import '../../../common_widgets/banner_widget.dart';
+import '../../../common_widgets/custom_button_widget.dart';
+import '../../../common_widgets/custom_text_button_widget.dart';
+import '../../../common_widgets/custom_text_field_widget.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
   static const String routeName = '/sign-up';
   const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.clear();
+    emailController.clear();
+    passwordController.clear();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +37,34 @@ class SignUpScreen extends StatelessWidget {
         ),
         child: SingleChildScrollView(
           child: Column(
-            children: const [
-              BannerWidget(),
+            children: [
+              const BannerWidget(),
+              CustomTextFieldWidget(
+                labelName: "Complete Name",
+                hintText: "Write your name",
+                controller: nameController,
+              ),
+              CustomTextFieldWidget(
+                labelName: "Email Address",
+                hintText: "Write your mail address",
+                controller: emailController,
+              ),
+              CustomTextFieldWidget(
+                labelName: "Password",
+                hintText: "Write your security",
+                controller: passwordController,
+                isObsecure: true,
+              ),
+              const SizedBox(height: AppSize.s16),
+              CustomButtonWidget(
+                title: "Get Started",
+                onTap: () {},
+              ),
+              const SizedBox(height: AppSize.s20),
+              CustomTextButtonWidget(
+                title: "Sign In to My Account",
+                onPressed: () {},
+              ),
             ],
           ),
         ),
--- lib/src/theme_manager/values_manager.dart
+++ lib/src/theme_manager/values_manager.dart
@@ -21,6 +21,7 @@ class AppPadding {
   static const double p4 = 4.0;
   static const double p8 = 8.0;
   static const double p12 = 12.0;
+  static const double p15 = 15.0;
   static const double p16 = 16.0;
   static const double p18 = 18.0;
   static const double p20 = 20.0;
@@ -41,6 +42,7 @@ class AppSize {
   static const double s6 = 6.0;
   static const double s8 = 8.0;
   static const double s12 = 12.0;
+  static const double s14 = 14.0;
   static const double s16 = 16.0;
   static const double s18 = 18.0;
   static const double s20 = 20.0;
@@ -49,4 +51,5 @@ class AppSize {
   static const double s28 = 28.0;
   static const double s36 = 36.0;
   static const double s40 = 40.0;
+  static const double s50 = 50.0;
 }
