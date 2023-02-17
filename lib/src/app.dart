d41886986b0bee05e0f09634b353070f729c8eaf
Ripat-2345 <ripatalsafar@gmail.com>
2023-02-17 14:38:39 +0700

    update implementasi auth bloc

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,33 @@
+import 'package:dating_app/src/features/authentication/data/data_user_account_local.dart';
 import 'package:dating_app/src/features/authentication/presentation/sign_up_screen.dart';
 import 'package:dating_app/src/theme_manager/theme_data_manager.dart';
 import 'package:flutter/material.dart';
 
-import 'features/authentication/presentation/sign_up_age_job_screen.dart';
 import 'features/authentication/presentation/sign_up_upload_photo_screen.dart';
 import 'features/likes_you/presentation/explore_people_screen.dart';
 import 'features/likes_you/presentation/people_loved_screen.dart';
 import 'features/likes_you/presentation/people_profile_screen.dart';
 
-class AppScreen extends StatelessWidget {
+class AppScreen extends StatefulWidget {
   const AppScreen({super.key});
+
+  @override
+  State<AppScreen> createState() => _AppScreenState();
+}
+
+class _AppScreenState extends State<AppScreen> {
+  bool isRegister = false;
+
+  isUserRegister() async {
+    isRegister = await UserAccountRegister.getUserAccountRegister();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isUserRegister();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +35,7 @@ class AppScreen extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       theme: getApplicationThemeData(),
-      initialRoute: SignUpScreen.routeName,
+      home: isRegister ? const ExplorePeopleScreen() : const SignUpScreen(),
       routes: {
         SignUpScreen.routeName: (context) => const SignUpScreen(),
         SignUpUploadPhotoScreen.routeName: (context) =>
--- lib/src/common_widgets/explore_people_app_bar_widget.dart
+++ lib/src/common_widgets/explore_people_app_bar_widget.dart
@@ -1,28 +1,44 @@
+import 'dart:io';
+
 import 'package:dating_app/src/common_widgets/logo_widget.dart';
-import 'package:dating_app/src/theme_manager/values_manager.dart';
+import 'package:dating_app/src/features/authentication/data/data_user_account_local.dart';
+import 'package:dating_app/src/features/authentication/presentation/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../features/likes_you/presentation/people_loved_screen.dart';
 import '../theme_manager/asset_image_icon_manager.dart';
 
 class ExplorePeopleAppBarWidget extends StatelessWidget {
-  const ExplorePeopleAppBarWidget({super.key});
+  final String? imagePath;
+  const ExplorePeopleAppBarWidget({required this.imagePath, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Container(
-          width: 55.0,
-          height: 55.0,
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            image: DecorationImage(
-              image: AssetImage(
-                "${AssetImageIconManager.assetPath}/user_image.png",
+        GestureDetector(
+          onTap: () {
+            UserAccountRegister.userAccountLogout();
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              SignUpScreen.routeName,
+              (route) => false,
+            );
+          },
+          child: Container(
+            width: 55.0,
+            height: 55.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: imagePath != null
+                    ? FileImage(File(imagePath!))
+                    : const AssetImage(
+                        "${AssetImageIconManager.assetPath}/user_image.png",
+                      ) as ImageProvider,
+                fit: BoxFit.cover,
               ),
-              fit: BoxFit.cover,
             ),
           ),
         ),
